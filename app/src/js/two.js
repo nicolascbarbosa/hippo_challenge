28ab5583469ebc481e5afc949dbf95ea85a6c53f
Nicolas C. Barbosa <nicolascb.claudio@gmail.com>
2017-05-28 23:15:47 -0300

    exercise two complete

--- app/src/js/two.js
+++ app/src/js/two.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const two = () => {
+  const doc = this.document;
+  const form = doc.querySelector('.js-year');
+  const input = doc.querySelector('.js-year-user');
+  const result = doc.querySelector('.exercise-two > .wrap > .result');
+
+  let checkYear = (year) => ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    result.textContent = '';
+
+    let year = input.value;
+    let isLeap = checkYear(year) ? `O ${year} é bissexto` : `O ${year} NÃO é bissexto`;
+    result.appendChild(doc.createTextNode(isLeap));
+
+  }, true);
+};
+
+two.call(window);
